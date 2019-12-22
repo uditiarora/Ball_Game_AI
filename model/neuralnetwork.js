f6100338afe610d4755efc65f18736c723976987
Uditi <uditiarora@gmail.com>
2019-12-22 19:07:45 +0530

    rearranging

--- model/neuralnetwork.js
+++ model/neuralnetwork.js
@@ -1,21 +1,4 @@
-// Other techniques for learning
 
-class ActivationFunction {
-  constructor(func, dfunc) {
-    this.func = func;
-    this.dfunc = dfunc;
-  }
-}
-
-let sigmoid = new ActivationFunction(
-  x => 1 / (1 + Math.exp(-x)),
-  y => y * (1 - y)
-);
-
-let tanh = new ActivationFunction(
-  x => Math.tanh(x),
-  y => 1 - (y * y)
-);
 
 
 class NeuralNetwork {
@@ -137,3 +120,20 @@ class NeuralNetwork {
     this.bias_o.map(func);
   }
 }
+
+class ActivationFunction {
+  constructor(func, dfunc) {
+    this.func = func;
+    this.dfunc = dfunc;
+  }
+}
+
+let sigmoid = new ActivationFunction(
+  x => 1 / (1 + Math.exp(-x)),
+  y => y * (1 - y)
+);
+
+let tanh = new ActivationFunction(
+  x => Math.tanh(x),
+  y => 1 - (y * y)
+);
